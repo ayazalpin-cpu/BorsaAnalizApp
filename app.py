0df4332a9a2de253f9f8c54775e2b24b3eb9a32d
ayazalpin-cpu <ayazalpin@gmail.com>
2026-06-14 14:50:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ import plotly.express as px
 # --- 1. YAPAY ZEKA VE SAYFA AYARLARI ---
 # Sistem internete yüklendiğinde şifreyi kasadan (secrets) okuyacak,
 # eğer yereldeysen tırnak içindeki kendi anahtarını kullanmaya devam edecek.
-try:
-   GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 genai.configure(api_key=GEMINI_API_KEY)
 
 st.set_page_config(page_title="Buffett AI Pro", layout="wide", page_icon="🚀")
